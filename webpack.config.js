48f8b4b03f8ec8c7d9c050363e59ab44edaa16d4
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-12-29 03:38:18 -0600

    config, loaders & plugins

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+//path => quiere decir donde vamos a trabajar o hubicar/utilizar este proyecto
+const path = require("path");
+//simplifica la creación de archivos HTML para servir a sus paquetes webpack
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//module.exports => objeto donde agregamos la configuracion del proyecto
+module.exports = {
+  //entry => hubicacion del punto de entrada
+  entry: "./src/index.js",
+  //output => donde vive el proyecto unas vez preparado
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  //extensiones que vamos a utilizar
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  //reglas a crear con nuestros loaders y plugins
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
